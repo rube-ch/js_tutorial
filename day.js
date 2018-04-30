b5e9a763be14ad64d16d72bc5f4452c9977c5738
Ruben Cornejo <rubech@gmail.com>
2018-04-30 17:41:38 +0000

    Palindrome tester usin node module

---
+++ day.js
@@ -0,0 +1,10 @@
+function dayOfTheWeek(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                         "Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
+}
+
+// Returns a greeting for the given date.
+function greeting(date) {
+  return `Hello, world! Happy ${dayOfTheWeek(date)}, now from a file!`
+}
